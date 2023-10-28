6bd7521f97bf536b1e47a86c0cbcbcad75baab75
user.email <developerjenzii@gmail.com>
2023-10-28 16:47:33 +0200

    add standard city

--- src/app.js
+++ src/app.js
@@ -5,18 +5,38 @@ const getWeather = async function (){
   try {
     let response =  await fetch(`https://api.weatherapi.com/v1/current.json?key=6e512773dd404c5d921214724232510&q=${await getUserInput()}&aqi=no`);
     let responseData = await response.json();
+    console.log(responseData);
     return responseData;
   } catch (error) {
-    console.error('oops');
+    console.error('oops', error);
   }
 }
 
 async function setWeather(weather){
   let paragraph = document.querySelector('p');
-  unit === 'C' ? paragraph.textContent =  ` ${weather.current.temp_c} celsius`: paragraph.textContent = `${weather.current.temp_f} fahrenheit`;
+  let txt =  `The weather in ${weather.location.name} is:`;
+  let uvtxt = `and the uv index is ${weather.current.uv} `;
+  try {
+    unit === 'C' ? paragraph.textContent =  ` ${txt} ${weather.current.temp_c} celsius ${uvtxt}`: paragraph.textContent = `${txt}  ${weather.current.temp_f} fahrenheit ${uvtxt}`;
+    if (weather.current.temp_c >= 20){
+      let weatherType = 'hot';
+      getImage(weatherType) 
+    } else if (weather.current.temp_c <= 20 && weather.current.precip_mm > 2){
+      weatherType = 'rain';
+      getImage(weatherType)
+    } else {
+      weatherType = 'cold';
+      getImage(weatherType)
+    }
+  } catch (error) {
+    console.error('you should input a valid city name ok');
+    document.querySelector('p').textContent = 'you should input a valid city name ok';
+  }
+  
 }
 
 async function getUserInput(){
+  if (document.querySelector('#search-location').value == '') return 'montreal';
   return document.querySelector('#search-location').value;
 }
 
@@ -34,3 +54,16 @@ document.addEventListener('keypress', async (e) => {
 
  const toggle = document.querySelector('.slider');
  toggle.addEventListener('click', async () => { toggleUnit(); setWeather(await getWeather())} );
+
+
+ img = document.querySelector('#weather-image');
+   function getImage(weatherType){ fetch(`https://api.giphy.com/v1/gifs/translate?api_key=0rAsbwH3i7285Zch6HeplahChR0fzjvJ&s=${weatherType}&weirdness=6`, {mode:'cors'} )
+    .then((response)=>{
+      return response.json();
+    })
+    .catch((error) => { console.error('woops something couldnt be fetched', error) } )
+    .then((response)=>{
+      if (response.data == 0) {console.log('common');throw new Error('oopsiedaisy, no gif could be found');}
+      Promise.resolve( img.src = response.data.images.original.url);
+    })
+    .catch((error) => { console.error('woops something couldnt be fetched', error) } )}
